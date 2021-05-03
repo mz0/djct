76e2a498f6f46a37969aef2c7ca9dcea3cc534fc
Mark Zhitomirski <marcuzero@gmail.com>
2021-05-03 23:51:24 +0300

    make me a dummy

---
+++ app.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    
+    const obj = document.getElementById('square');
+    obj.addEventListener('submit', handleSubmitForm);
+    // window.addEventListener("keydown", event => {
+    //   let kp = 'E';
+    //   switch (event.key) {
+    //     case 'd':
+    //     case 's':
+    //     case 'a':
+    //     case 'w':
+    //       kp = event.key;
+    //       handleKey(obj, kp);
+    //   }
+    // }, false);
+}
