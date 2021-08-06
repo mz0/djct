36204646848707426e41198b9e43afdce8567cf3
Mark Zhitomirski <marcuzero@gmail.com>
2021-08-06 14:20:06 +0300

    2021-05-09 explore native (event-based) indexedDB API

--- app.js
+++ app.js
@@ -10,3 +10,40 @@ window.onload = function() {
     const form = document.getElementById('dict');
     form.addEventListener('submit', getWord);
 }
+
+let openRequest = indexedDB.open("words", 1);
+openRequest.onupgradeneeded = function(evt) {
+    // if the client had no DB, do init
+    let db = openRequest.result;
+    // switch(evt.oldVersion) { // existing db version
+    //   case 0:
+    //     // version 0 means that the client had no database
+    if (!db.objectStoreNames.contains('words')) {
+        db.createObjectStore('words', {keyPath: 'wrd'});
+        console.log("'words' created", openRequest.result);
+    } else {
+        console.log("Surprise! 'words' found!", openRequest.result);
+    }
+        // let transaction = db.transaction("words", "readwrite");
+        // let words = transaction.objectStore("words");
+        // let w1 = { wrd: 'a', article: 'a - an indefinite article'}
+        // let w2 = { wrd: 'aa', article: '"aa" is unknown'}
+        // let request = words.add(w1);
+        // request.onsuccess = function() { // (4)
+        //     console.log("word added to the store", request.result);
+        // };
+        // request.onerror = function() {
+        //     console.log("Error", request.error);
+        // };
+        // request = words.add(w2);
+    // }
+};
+
+openRequest.onerror = function() {
+    console.error("Error", openRequest.error);
+};
+
+openRequest.onsuccess = function() {
+    let db = openRequest.result;
+    // continue working with database using db object
+};
